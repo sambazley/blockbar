245303995977346220262a5d3a8dc23945a034d4
Sam Bazley <sambazley@protonmail.com>
2018-08-14 18:48:19 +0100

    Added missing bbc.h

---
+++ src/bbc.h
@@ -0,0 +1,13 @@
+#ifndef BBC_H
+#define BBC_H
+
+#define socketpath "/tmp/blockbar-socket"
+#define bbcbuffsize 1024
+
+#define setout 1
+#define setret 2
+
+#define rstdout 1
+#define rstderr 2
+
+#endif /* BBC_H */
