112764ac7c04197d5375fcfd4520acf356677a04
Sam Bazley <sambazley@protonmail.com>
2019-01-05 01:32:58 +0000

    Rewrote zsh autocomplete, removed fish autocomplete

--- autocomplete/bbc.zsh
+++ autocomplete/bbc.zsh
@@ -1,5 +1,210 @@
 #compdef _bbc bbc
 
+_list_blocks() {
+    ids=()
+    bbc list | while read id execpath; do
+        if [[ "$1" == "eachmon" ]]; then
+            xrandr | grep " connected" | awk '{print $1}' | while read disp; do
+                ids+=$(echo "$disp\\:$id:$execpath")
+            done
+        fi
+        ids+="$id:$execpath"
+    done
+    _describe -V 'block ids' ids
+}
+
+_list_properties() {
+    props=()
+    bbc list-properties | while read t n d; do
+        props+="$n:$d"
+    done
+    _describe 'properties' props
+}
+
+_list_settings() {
+    settings=()
+    module=
+    bbc list-settings | while read t n d; do
+        [[ -z "$t" ]] && continue
+
+        if [[ "$t" =~ :$ ]]; then
+            module="${t:0:-1}\\:"
+            continue
+        fi
+
+        settings+="$module$n:$d"
+    done
+    _describe -V 'settings' settings
+}
+
+_list_modules() {
+    _values 'modules' $(bbc list-modules | awk '{print $1}')
+}
+
+_complete_property_value() {
+    property=$words[$(( $CURRENT - 1 ))]
+    t=$(bbc list-properties | awk -v p=$property '$2==p {print $1}')
+
+    case $property in
+    module)
+        _list_modules
+        return
+        ;;
+    exec)
+        _path_files -P "/" -W "/"
+        return
+        ;;
+    esac
+
+    case $t in
+    bool)
+        _values 'boolean' 'true' 'false'
+        return
+        ;;
+    position)
+        _values 'position' 'left' 'center' 'right'
+        return
+        ;;
+    esac
+}
+
+_complete_setting_value() {
+    setting=$words[$(( $CURRENT - 1 ))]
+
+    case $setting in
+    position)
+        _values 'position' 'top' 'bottom'
+        return
+        ;;
+    trayside)
+        _values 'position' 'left' 'right'
+        return
+        ;;
+    esac
+
+    module=
+    vt=
+
+    bbc list-settings | while read t n d; do
+        [[ -z "$t" ]] && continue
+
+        if [[ "$t" =~ :$ ]]; then
+            module="${t:0:-1}:"
+            continue
+        fi
+
+        if [[ "$setting" == "$module$n" ]]; then
+            vt=$t
+            break
+        fi
+    done
+
+    case $vt in
+    bool)
+        _values 'boolean' 'true' 'false'
+        return
+        ;;
+    position)
+        _values 'position' 'left' 'center' 'right'
+        return
+        ;;
+    esac
+}
+
+_comp_exec() {
+    case $CURRENT in
+    3)
+        _list_blocks
+        ;;
+    esac
+}
+
+_comp_property() {
+    case $CURRENT in
+    3)
+        _list_blocks eachmon
+        ;;
+    4)
+        _list_properties
+        ;;
+    5)
+        _complete_property_value
+        ;;
+    esac
+}
+
+_comp_setting() {
+    case $CURRENT in
+    3)
+        _list_settings
+        ;;
+    4)
+        _complete_setting_value
+        ;;
+    esac
+}
+
+_comp_new() {
+    case $CURRENT in
+    3)
+        _values 'options' '--eachmon'
+        ;;
+    esac
+}
+
+_comp_rm() {
+    case $CURRENT in
+    3)
+        _list_blocks
+        ;;
+    esac
+}
+
+_comp_move-left() {
+    case $CURRENT in
+    3)
+        _list_blocks
+        ;;
+    esac
+}
+
+_comp_move-right() {
+    case $CURRENT in
+    3)
+        _list_blocks
+        ;;
+    esac
+}
+
+_comp_dump() {
+    case $CURRENT in
+    3)
+        _values 'options' '--explicit'
+        ;;
+    esac
+}
+
+_comp_load-module() {
+    case $CURRENT in
+    3)
+        _path_files -P "/" -W "/"
+        ;;
+    esac
+}
+
+_comp_unload-module() {
+    case $CURRENT in
+    3)
+        _list_modules
+        ;;
+    esac
+}
+
+_exists() {
+    declare -f -F $1 > /dev/null
+    return $?
+}
+
 _bbc()
 {
     if [[ "$CURRENT" == "2" ]]; then
@@ -8,62 +213,7 @@ _bbc()
             cmds+=$(echo "$line" | sed "s/^\s*//g" | sed -E "s/\s.*\s{2,}/:/g")
         done
         _describe -t commands 'bbc' cmds
-    elif [[ "$CURRENT" == "3" && "${words[2]}" =~ "(exec)|(property)|\
-            (rm)|(move-left)|(move-right)" ]]; then
-        ids=()
-        bbc list | while read id; do
-            ids+=$(echo $id | sed -E "s/\t/:/g")
-        done
-        _describe -t commands 'bbc' ids
-    elif [[ "$CURRENT" == "3" && "${words[2]}" == "setting" ]]; then
-        settings=()
-        bbc list-settings | while read s; do
-            settings+=$(echo $s | sed 's/^[a-zA-Z]*\s*//g' | sed 's/\s\{2,\}/:/g')
-        done
-        _describe -t commands 'bbc' settings
-    elif [[ "$CURRENT" == "3" && "${words[2]}" == "new" ]]; then
-        list=("--eachmon")
-        _describe -t commands 'bbc' list
-    elif [[ "$CURRENT" == "3" && "${words[2]}" == "dump" ]]; then
-        list=("--explicit")
-        _describe -t commands 'bbc' list
-    elif [[ "$CURRENT" == "3" && "${words[2]}" == "load-module" ]]; then
-        _path_files -P "/" -W "/"
-    elif [[ "$CURRENT" == "3" && "${words[2]}" == "unload-module" ]]; then
-        mods=()
-        bbc list-modules | while read m; do
-            mods+=$(echo $m | awk '{print $1}')
-        done
-        _describe -t commands 'bbc' mods
-    elif [[ "$CURRENT" == "4" && "${words[2]}" == "property" ]]; then
-        props=()
-        bbc list-properties | while read prop; do
-            props+=$(echo $prop | sed 's/^[a-zA-Z]*\s*//g' | sed 's/\s\{2,\}/:/g')
-        done
-        _describe -t commands 'bbc' props
-    elif [[ "$CURRENT" == "4" && "${words[2]}" == "setting" ]]; then
-        if [[ "${words[3]}" == "position" ]]; then
-            list=("top" "bottom")
-            _describe -t commands 'bbc' list
-        elif [[ "${words[3]}" == "traybar" ]]; then
-            list=($(xrandr | grep "\sconnected" | awk '{print $1}'))
-            _describe -t commands 'bbc' list
-        elif [[ "${words[3]}" == "trayside" ]]; then
-            list=("left" "right")
-            _describe -t commands 'bbc' list
-        fi
-    elif [[ "$CURRENT" == "5" && "${words[2]}" == "property" ]]; then
-        if [[ "${words[4]}" == "pos" ]]; then
-            list=("left" "center" "right")
-            _describe -t commands 'bbc' list
-        elif [[ "${words[4]}" == "exec" ]]; then
-            _path_files -P "/" -W "/"
-        elif [[ "${words[4]}" == "module" ]]; then
-            mods=()
-            bbc list-modules | while read m; do
-                mods+=$(echo $m | awk '{print $1}')
-            done
-            _describe -t commands 'bbc' mods
-        fi
+    else
+        _exists _comp_$words[2] && _comp_$words[2]
     fi
 }
